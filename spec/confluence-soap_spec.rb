18ea1447bb0969be33675e35edd698a0b4c133a0
Eric Himmelreich <eric@rawsyntax.com>
2014-01-22 14:24:53 -0500

    Clean up specs

--- spec/confluence-soap_spec.rb
+++ spec/confluence-soap_spec.rb
@@ -1,6 +1,6 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'spec_helper'
 
-describe "ConfluenceSoap" do
+describe ConfluenceSoap do
   let (:url) {"http://example.com?wsdl"}
   subject {ConfluenceSoap.new(url, 'user', 'password')}
 
